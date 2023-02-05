ea401803d1362b7197e5525665ad3b1161d03168
Andre-FOliveira <andre.faustino@hotmail.com>
2023-02-05 18:14:08 -0300

    Subindo o projeto Clima tempo para o gitHub

---
+++ scripts.js
@@ -0,0 +1,36 @@
+
+let chave = "92559c27a7ada31d5a4a69f4ea980e00"
+
+
+function colocarNaTela(dados) {
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = dados.name + " - " + dados.sys.country
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".sensacao-termica").innerHTML = "Sensação Termica " + Math.floor(dados.main.feels_like) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade do ar " + dados.main.humidity + "%"
+
+}
+
+async function buscarCidade(cidade) {
+
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
+        cidade +
+        "&appid=" +
+        chave +
+        "&lang=pt_br" +
+        "&units=metric"
+    )
+        .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+
+}
+
+function pesquisa() {
+    let cidade = document.querySelector(".input-city").value
+
+    buscarCidade(cidade)
+}
